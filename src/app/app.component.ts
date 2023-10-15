f55f3561492ee4d7673000c2a9b74504b248ed58
Marco Pinto <marcopinto01@icloud.com>
2023-10-15 03:00:20 +0200

    scroll e menu che si chiude per il mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,4 +17,45 @@ export class AppComponent implements AfterViewInit {
       this.chiSiamoAnchor.nativeElement.scrollIntoView();
     }
   }
+
+  constructor(private el: ElementRef) { }
+
+  scrollToSection(sectionId: string) {
+    const section = this.el.nativeElement.querySelector(sectionId);
+
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+      this.smoothScrollBehavior(section, 3000); 
+      this.closeMenu();
+    }
+  }
+
+  private smoothScrollBehavior(element: HTMLElement, duration: number) {
+    const start = element.scrollTop;
+    const startTime = performance.now();
+
+    function step(time: number) {
+      const currentTime = time - startTime;
+      const scrollDistance = element.scrollHeight;
+      const easeInOut = 0.5 - Math.cos(currentTime / duration * Math.PI) / 2;
+
+      element.scrollTop = start + scrollDistance * easeInOut;
+
+      if (currentTime < duration) {
+        requestAnimationFrame(step);
+      }
+    }
+
+    requestAnimationFrame(step);
+  }
+
+  closeMenu() {
+    const menuCheckbox = document.getElementById('menu-btn') as HTMLInputElement;
+
+    if (menuCheckbox) {
+      menuCheckbox.checked = false; // Chiudi la tendina del menu
+    }
+  }
+
+
 }
